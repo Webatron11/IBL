1b9af5ca460fd342a0ffaff14d13d03b977fac34
Oscar <oscarwebb162@protonmail.com>
2022-11-05 09:32:51 +1100

    Adding some stuff

--- Assets/Gps.cs
+++ Assets/Gps.cs
@@ -12,26 +12,30 @@ public class Gps : MonoBehaviour {
     private TcpClient connect = new TcpClient();
     public TMP_Text altText;
     
-    void Start() {
+    void Start()
+    {
         Debug.Log("Started");
-        IPAddress ip = IPAddress.Parse("192.168.1.127");
-        IPEndPoint endpoint = new IPEndPoint(ip, 6000);
+        altText.text = "N/A";
+        IPAddress ip = IPAddress.Parse("192.168.234.72");
+        IPEndPoint endpoint = new IPEndPoint(ip, 6001);
         connect.Connect(endpoint);
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         Stream stream = connect.GetStream();
         var device = new StreamDevice(stream);
         device.MessageReceived += device_NmeaMessageReceived;
         device.OpenAsync();
+        altText.ForceMeshUpdate();
     }
     
     private void device_NmeaMessageReceived(object sender, NmeaMessageReceivedEventArgs args) {
         // called when a message is received
         if(args.Message is NmeaParser.Messages.Gga gga) {
+            altText.text = $"{gga.Altitude}";
             Debug.Log($"Your current location is: {gga.Latitude} , {gga.Longitude} at height , {gga.Altitude}");
-            altText.text = gga.Altitude.ToString();
         }
     }
 }
