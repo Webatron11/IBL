3696cc964ac53674662b259dead1bddbcd6442c2
Khorneflaeks <oscarwebb162@protonmail.com>
2022-11-07 22:08:28 +1100

    AAAA

--- Assets/Character/Base.cs
+++ Assets/Character/Base.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Base : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Base : MonoBehaviour {
+    public GameObject character;
+    public Vector2 location;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 }
--- Assets/Gps.cs
+++ Assets/Gps.cs
@@ -1,41 +1,104 @@
 // using System.Collections;
 // using System.Collections.Generic;
 
+using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
 using NmeaParser;
 using TMPro;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Gps : MonoBehaviour {
-    private TcpClient connect = new TcpClient();
+    private readonly TcpClient _connect = new TcpClient();
+    private readonly IPEndPoint _endpoint = new IPEndPoint(IPAddress.Parse("192.168.1.127"), 6000);
     public TMP_Text altText;
-    
+    public Transform character;
+    private double _oldlat;
+    private float _newlat;
+    private double _oldlong;
+    private float _newlong;
+
     void Start()
     {
         Debug.Log("Started");
         altText.text = "N/A";
-        IPAddress ip = IPAddress.Parse("192.168.234.72");
-        IPEndPoint endpoint = new IPEndPoint(ip, 6001);
-        connect.Connect(endpoint);
+        _connect.Connect(_endpoint);
+        character.position = Vector3.zero;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Stream stream = connect.GetStream();
+    void Update() {
+        Stream stream = Stream.Null;
+        try {
+            stream = _connect.GetStream();
+        }
+        catch (InvalidOperationException) {
+            _connect.Connect(_endpoint);
+        }
+        
         var device = new StreamDevice(stream);
         device.MessageReceived += device_NmeaMessageReceived;
         device.OpenAsync();
+        Move();
         altText.ForceMeshUpdate();
     }
     
     private void device_NmeaMessageReceived(object sender, NmeaMessageReceivedEventArgs args) {
         // called when a message is received
         if(args.Message is NmeaParser.Messages.Gga gga) {
-            altText.text = $"{gga.Altitude}";
-            Debug.Log($"Your current location is: {gga.Latitude} , {gga.Longitude} at height , {gga.Altitude}");
+            Debug.Log($"Your current location is: {gga.Latitude} , {gga.Longitude} at height , {Math.Round(gga.Altitude)}");
+            _oldlat = _newlat;
+            _newlat = (float)gga.Latitude;
+            _oldlong = _newlong;
+            _newlong = (float)gga.Longitude;
+            altText.text = $"{Math.Round(gga.Altitude)}";
         }
+    }
+
+    private void Move() {
+        /*float speed = 1f;
+        Vector3 moveDir = new Vector3();
+        
+        if (Comparator(_newlat, _oldlat) == true) {
+            moveDir = Vector3.right;
+            //speed = Delta(_newlat, _oldlat);
+        }
+        if (Comparator(_newlat, _oldlat) == false) {
+            moveDir = Vector3.left;
+            //speed = Delta(_newlat, _oldlat);
+        }
+        if (Comparator(_newlong, _oldlong) == true) {
+            moveDir = Vector3.up;
+            //speed = Delta(_newlong, _oldlong);
+        }
+        if (Comparator(_newlong, _oldlong) == false) {
+            moveDir = Vector3.down;
+            //speed = Delta(_newlong, _oldlong);
+        }
+        if (Comparator(_newlong, _oldlong) == null && Comparator(_newlat, _oldlat) == null) {
+            moveDir = Vector3.zero;
+            //speed = 0f;
+        }
+        
+        character.position += moveDir.normalized * speed;*/
+
+        Vector2 coords = new Vector2(_newlat, _newlong);
+        character.position = coords;
+    }
+
+    private static float Delta(double big, double small) {
+        return (float)(big - small);
+    }
+    private bool? Comparator(double in1, double in2) {
+        if (in1 > in2) {
+            return true;
+        } 
+        if (in1 < in2) {
+            return false;
+        }
+        return null;
     }
 }
